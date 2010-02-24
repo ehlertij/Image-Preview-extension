00d46421a184b3ee271574e31e1f48a463763137
Ian Ehlert <ehlertij@gmail.com>
2010-02-23 22:50:50 -0600

    Added youtube support. Added Google images support. Fixed issue with wikipedia links. Fixed issue with links ending in image filtypes that weren't links to images.

--- image_preview_page_script.js
+++ image_preview_page_script.js
@@ -1,21 +1,34 @@
-var showImagePreview = function(path){
-  var image = document.getElementById('image_preview_image_tag');
-  var mask = document.getElementById('ip_mask');
-  var modal = document.getElementById('ip_modal');
-  
-  image.setAttribute("src", path);
-  
-  mask.style.display = "block";
-  modal.style.display = "block";
-  //document.body.style.overflow = 'hidden';
-  return false;
+var ip_showImagePreview = function(path){
+	var image = document.getElementById('image_preview_image_tag');
+  	image.setAttribute("src", path);
+  	ip_displayModal();
+  	return false;
 }
 
-var hideImagePreview = function(){
+var ip_hideModal = function(){
 	var mask = document.getElementById("ip_mask");
 	var modal = document.getElementById("ip_modal");
+	var youtube_div = document.getElementById("youtube_preview_div_tag");
 	
+	youtube_div.innerHTML = "";
 	mask.style.display = "none";
 	modal.style.display = "none";
-	//document.body.style.overflow = 'auto';
+}
+
+var ip_showYoutubePreview = function(path){
+	var youtube_div = document.getElementById('youtube_preview_div_tag');
+	var youtubeEmbed = "<object width='560' height='340'><param name='movie' value='" + path + "&fs=1'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param>";
+	youtubeEmbed += "<embed src='" + path + "&fs=1' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='560' height='340'></embed></object>";
+	
+	youtube_div.innerHTML = youtubeEmbed;
+	
+	ip_displayModal();
+	return false;
+}
+
+var ip_displayModal = function(){
+	var mask = document.getElementById('ip_mask');
+	var modal = document.getElementById('ip_modal');
+	mask.style.display = "block";
+  	modal.style.display = "block";
 }
--- ip_content_script.js
+++ ip_content_script.js
@@ -1,12 +1,39 @@
 var parsePage = function(){
 	var found;
-	var re = /.(png|jpg|jpeg|gif|bmp)$/i
+	var image_re = /(.png|.jpg|.jpeg|.gif|.bmp)$/i
+	var youtube_re = /(?:\S+?youtube\S+watch\S+v=?[A-Za-z0-9_-]{11})/i
+	var googleimages_re = /imgres(?:\S+imgurl=)/i
+	var wiki_re = /\/File:/i
+	
 	var anchors = document.getElementsByTagName("a");
 	for (var i = 0; i < anchors.length; ++i) {
 	  var child = anchors[i];
-	  if (child.href.match(re)) {
+	  if (child.href.match(image_re) && !child.href.match(wiki_re)) {
 		var imagePath = child.href;
-		child.setAttribute('onclick', 'return showImagePreview("' + imagePath + '")');
+		child.setAttribute('onclick', 'return ip_showImagePreview("' + imagePath + '")');
+		found = true;
+	  } else if (child.href.match(youtube_re)) {
+	  	var youtubePath = "http://www.youtube.com/v/";
+	  	var start = child.href.indexOf('v=') + 2;
+	  	
+	  	if(child.href.indexOf('&', start) != -1){
+	  		var end = child.href.indexOf('&', start) - start;
+	  		youtubePath += child.href.substr(start,end);
+  		} else {
+  			youtubePath += child.href.substr(start);
+  		}
+	  	child.setAttribute('onclick', 'return ip_showYoutubePreview("' + youtubePath + '")');
+	  	found = true;
+	  } else if (child.href.match(googleimages_re)){
+		var start = child.href.indexOf('imgurl=') + 7;
+		var imagePath = "";
+		if(child.href.indexOf('&', start) != -1){
+			var end = child.href.indexOf('&', start) - start;
+			imagePath = child.href.substr(start, end);
+		} else {
+			imagePath = child.href.substr(start);
+		}
+		child.setAttribute('onclick', 'return ip_showImagePreview("' + imagePath + '")');
 		found = true;
 	  }
 	}
@@ -16,28 +43,27 @@ var parsePage = function(){
 		script.type = "text/javascript";
 		document.body.appendChild(script);
 
-		/*var cssLink = document.createElement('link');
-		cssLink.setAttribute("rel", "stylesheet");
-		cssLink.setAttribute("type", "text/css");
-		cssLink.setAttribute("href", chrome.extension.getURL("image_preview.css"));
-		document.getElementsByTagName("head")[0].appendChild(cssLink);
-		*/
 		var mask = document.createElement('div');
 		mask.setAttribute("id", "ip_mask");
 		mask.setAttribute("style", "position:fixed; z-index:9000000; left: 0px; top: 0px; height: 100%; width: 100%; background-color:#000; display:none; opacity: 0.75;");
-		mask.setAttribute("onclick", "javascript:hideImagePreview()");
+		mask.setAttribute("onclick", "javascript:ip_hideModal()");
 
 		var modal = document.createElement('div');
 		modal.setAttribute("id", "ip_modal");
 		modal.setAttribute("style", "position:fixed; margin-left:auto; width: 100%; height: 100%; top: 0px; padding: 30px 0px; margin-right:auto; display:none; z-index:9999999;");
-		modal.setAttribute("onclick", "javascript:hideImagePreview()");
+		modal.setAttribute("onclick", "javascript:ip_hideModal()");
 
 		var image = document.createElement('img');
 		image.setAttribute('id', 'image_preview_image_tag');
 		image.setAttribute("style", "margin-left:auto; margin-right:auto; max-width: 95%; max-height: 95%; text-align: center; display: block; position: relative;");
 		image.setAttribute("align", "middle");
 
+		var youtube = document.createElement('div');
+		youtube.setAttribute('id', 'youtube_preview_div_tag');
+		youtube.setAttribute("align", "middle");
+
 		modal.appendChild(image);
+		modal.appendChild(youtube);
 		document.body.appendChild(mask);
 		document.body.appendChild(modal);
 	}
