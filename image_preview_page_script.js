3165c3d71156a69fb204136c819cf449db49f461
Ian Ehlert <ehlertij@gmail.com>
2010-02-26 23:54:03 -0600

    Updated manifest.json; Commiting files I left out;

--- image_preview_page_script.js
+++ image_preview_page_script.js
@@ -1,6 +1,9 @@
+var ip_zoomed = false;
+
 var ip_showImagePreview = function(path){
 	var image = document.getElementById('image_preview_image_tag');
   	image.setAttribute("src", path);
+  	image.setAttribute("style", "cursor:pointer; margin-left:auto; margin-right:auto; max-width: 95%; max-height: 95%; text-align: center; display: block; position: relative; z-index:9999999;");
   	ip_displayModal();
   	return false;
 }
@@ -9,19 +12,26 @@ var ip_hideModal = function(){
 	var mask = document.getElementById("ip_mask");
 	var modal = document.getElementById("ip_modal");
 	var youtube_div = document.getElementById("youtube_preview_div_tag");
+	var image = document.getElementById('image_preview_image_tag');
 	
 	youtube_div.innerHTML = "";
+	image.style.display = "none";
 	mask.style.display = "none";
 	modal.style.display = "none";
+	ip_zoomed = false;
 }
 
 var ip_showYoutubePreview = function(path){
 	var youtube_div = document.getElementById('youtube_preview_div_tag');
-	var youtubeEmbed = "<object width='560' height='340'><param name='movie' value='" + path + "&fs=1'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param>";
-	youtubeEmbed += "<embed src='" + path + "&fs=1' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='560' height='340'></embed></object>";
+	var modal = document.getElementById('ip_modal');
+	var width = window.innerWidth * .7;
+	var height = width * .6;
+	var youtubeEmbed = "<object width='" + width + "' height='" + height + "'><param name='movie' value='" + path + "&fs=1&hd=1&autoplay=1'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param>";
+	youtubeEmbed += "<embed src='" + path + "&fs=1&hd=1&autoplay=1' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='" + width + "' height='" + height + "'></embed></object>";
 	
 	youtube_div.innerHTML = youtubeEmbed;
 	
+	modal.style.position = "fixed";
 	ip_displayModal();
 	return false;
 }
@@ -31,4 +41,19 @@ var ip_displayModal = function(){
 	var modal = document.getElementById('ip_modal');
 	mask.style.display = "block";
   	modal.style.display = "block";
+  	modal.focus();
+}
+
+var ip_zoomImage = function(){
+	var image = document.getElementById('image_preview_image_tag');
+	var modal = document.getElementById('ip_modal');
+	if(ip_zoomed == false){
+		image.setAttribute("style", "cursor:pointer; margin-left:auto; margin-right:auto; text-align: center; display: block; position: absolute; z-index:9999999;");
+		modal.style.position = "absolute";
+		ip_zoomed = true;
+	} else {
+		image.setAttribute("style", "cursor:pointer; margin-left:auto; margin-right:auto; max-width: 95%; max-height: 95%; text-align: center; display: block; position: relative; z-index:9999999;");
+		modal.style.position = "fixed";
+		ip_zoomed = false;
+	}
 }
--- ip_content_script.js
+++ ip_content_script.js
@@ -1,4 +1,4 @@
-var parsePage = function(){
+function parsePage(response){
 	var found;
 	var image_re = /(.png|.jpg|.jpeg|.gif|.bmp)$/i
 	var youtube_re = /(?:\S+?youtube\S+watch\S+v=?[A-Za-z0-9_-]{11})/i
@@ -8,14 +8,13 @@ var parsePage = function(){
 	var anchors = document.getElementsByTagName("a");
 	for (var i = 0; i < anchors.length; ++i) {
 	  var child = anchors[i];
-	  if (child.href.match(image_re) && !child.href.match(wiki_re)) {
+	  if (response.image_enabled && child.href.match(image_re) && !child.href.match(wiki_re)) {
 		var imagePath = child.href;
 		child.setAttribute('onclick', 'return ip_showImagePreview("' + imagePath + '")');
 		found = true;
-	  } else if (child.href.match(youtube_re)) {
+	  } else if (response.youtube_enabled && child.href.match(youtube_re)) {
 	  	var youtubePath = "http://www.youtube.com/v/";
 	  	var start = child.href.indexOf('v=') + 2;
-	  	
 	  	if(child.href.indexOf('&', start) != -1){
 	  		var end = child.href.indexOf('&', start) - start;
 	  		youtubePath += child.href.substr(start,end);
@@ -24,7 +23,7 @@ var parsePage = function(){
   		}
 	  	child.setAttribute('onclick', 'return ip_showYoutubePreview("' + youtubePath + '")');
 	  	found = true;
-	  } else if (child.href.match(googleimages_re)){
+	  } else if (response.image_enabled && child.href.match(googleimages_re)){
 		var start = child.href.indexOf('imgurl=') + 7;
 		var imagePath = "";
 		if(child.href.indexOf('&', start) != -1){
@@ -38,35 +37,43 @@ var parsePage = function(){
 	  }
 	}
 	if(found){
-		var script = document.createElement('script');
-		script.src = chrome.extension.getURL("image_preview_page_script.js");
-		script.type = "text/javascript";
-		document.body.appendChild(script);
-
-		var mask = document.createElement('div');
-		mask.setAttribute("id", "ip_mask");
-		mask.setAttribute("style", "position:fixed; z-index:9000000; left: 0px; top: 0px; height: 100%; width: 100%; background-color:#000; display:none; opacity: 0.75;");
-		mask.setAttribute("onclick", "javascript:ip_hideModal()");
-
-		var modal = document.createElement('div');
-		modal.setAttribute("id", "ip_modal");
-		modal.setAttribute("style", "position:fixed; margin-left:auto; width: 100%; height: 100%; top: 0px; padding: 30px 0px; margin-right:auto; display:none; z-index:9999999;");
-		modal.setAttribute("onclick", "javascript:ip_hideModal()");
-
-		var image = document.createElement('img');
-		image.setAttribute('id', 'image_preview_image_tag');
-		image.setAttribute("style", "margin-left:auto; margin-right:auto; max-width: 95%; max-height: 95%; text-align: center; display: block; position: relative;");
-		image.setAttribute("align", "middle");
-
-		var youtube = document.createElement('div');
-		youtube.setAttribute('id', 'youtube_preview_div_tag');
-		youtube.setAttribute("align", "middle");
-
-		modal.appendChild(image);
-		modal.appendChild(youtube);
-		document.body.appendChild(mask);
-		document.body.appendChild(modal);
+		addModal();
 	}
 }
 
-parsePage();
+function addModal(){
+	var script = document.createElement('script');
+	script.src = chrome.extension.getURL("image_preview_page_script.js");
+	script.type = "text/javascript";
+	document.body.appendChild(script);
+
+	var mask = document.createElement('div');
+	mask.setAttribute("id", "ip_mask");
+	mask.setAttribute("style", "position:fixed; z-index:9000000; left: 0px; top: 0px; height: 100%; width: 100%; background-color:#000; display:none; opacity: 0.75;");
+	mask.setAttribute("onclick", "javascript:ip_hideModal()");
+
+	var modal = document.createElement('div');
+	modal.setAttribute("id", "ip_modal");
+	modal.setAttribute("style", "position:fixed; margin-left:auto; top: 0px; margin-right:auto; display:none; left:0; right:0;");
+
+	var image = document.createElement('img');
+	image.setAttribute('id', 'image_preview_image_tag');
+	image.setAttribute("style", "cursor:pointer; margin-left:auto; margin-right:auto; left:0; right:0; max-width: 95%; max-height: 95%; text-align: center; display: block; position: absolute; z-index:9999999;");
+	image.setAttribute("align", "middle");
+	image.setAttribute("onclick", "javascript:ip_zoomImage()");
+
+	var youtube = document.createElement('div');
+	youtube.setAttribute('id', 'youtube_preview_div_tag');
+	youtube.setAttribute("align", "middle");
+
+	modal.appendChild(image);
+	modal.appendChild(youtube);
+	document.body.appendChild(mask);
+	document.body.appendChild(modal);
+}
+
+var run = function(){
+	chrome.extension.sendRequest({func: "getOptions"}, parsePage);
+}
+
+run();
